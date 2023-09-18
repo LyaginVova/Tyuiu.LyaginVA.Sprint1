e4a07dc7e0fb5870914bba5f4e0eb265ec705895
Tyuiu.LyaginVA@outlook.com <vova.lyagin.2016@mail.ru>
2023-09-18 20:42:43 +0500

    Add project files.

---
+++ Tyuiu.LyaginVA.Sprint1.Task0.V28.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.LyaginVA.Sprint1.Task0.V28.Lib;
+
+namespace Tyuiu.LyaginVA.Sprint1.Task0.V28.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            var res = ds.Calculate();
+            Assert.AreEqual(2, res);
+        }
+    }
+}
